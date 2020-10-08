80e29b866a4334a2f5fb558ec26333b469b43556
Your name here <ctrumbo@uncc.edu>
2020-10-08 11:09:25 -0400

    added cors

--- Server.js
+++ Server.js
@@ -1,23 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
+app.use(cors());
 
+//app.use('/', express.static('public'));
 
+//const budget = require("./budgetData.json");
 
-
-
-const budget = require("./budgetData.json");
-
-app.get('/hello', (req, res) => {
-    res.send('hello world');
-});
+//app.get('/hello', (req, res) => {
+//    res.send('hello world');
+//});
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    res.sendFile('budgetData.json', {root: __dirname});
 });
 
 app.listen(port, () => {
-    console.log('example app listening at http://localhost:3000');
+    console.log('API Served at http://localhost:3000');
 });
